06e64659f3feb7f2a96a2237868782063e6c74a6
Asif <58340275+fullshow1337@users.noreply.github.com>
2020-01-20 17:03:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// homework js1
+var name = prompt('Введите имя');
+console.log(name);
+var age = prompt('Введите возраст');
+console.log(age);
+var addition = prompt('6 + 3 = ?');
+console.log(addition);
+var subtraction = prompt('6 - 3 = ?');
+console.log(subtraction);
+var multiplication = prompt('6 * 3 = ?');
+console.log(multiplication);
+var division = prompt('6 / 3 = ?');
+console.log(division);
+var divisioN = prompt('22 % 7 = ?')
+console.log(divisioN);
+
+
+
+
+
+
+
