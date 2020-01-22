d1a2d4ec5cc36a78c1533ffa0237440e2499fbad
Asif <58340275+fullshow1337@users.noreply.github.com>
2020-01-22 22:27:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // homework js1
-var name = prompt('Введите имя');
-console.log(name);
-var age = prompt('Введите возраст');
-console.log(age);
-var addition = prompt('6 + 3 = ?');
-console.log(addition);
-var subtraction = prompt('6 - 3 = ?');
-console.log(subtraction);
-var multiplication = prompt('6 * 3 = ?');
-console.log(multiplication);
-var division = prompt('6 / 3 = ?');
-console.log(division);
-var divisioN = prompt('22 % 7 = ?')
-console.log(divisioN);
+let userName = prompt('Введите имя', '');
+let userAge = prompt('Введите возраст', '');
+
+let addition = prompt('6 + 3 = ?', '');
+let substraction = prompt('6 - 3 = ?', '');
+let multiplication = prompt('6 * 3 = ?', '');
+let division = prompt('6 / 3 = ?', '');
+let residue = prompt('6 % 3 = ?', '');
+
+alert('Откройте консоль');
+console.log('Привет ' + userName);
+console.log('Ваш результат:')
+console.log('6 + 3 = ' + (6 + 3) + ' (Ваш ответ: ' + addition + ')' );
+console.log('6 - 3 = ' + (6 - 3) + ' (Ваш ответ: ' + substraction + ')' );
+console.log('6 * 3 = ' + (6 * 3) + ' (Ваш ответ: ' + multiplication + ')' );
+console.log('6 / 3 = ' + (6 / 3) + ' (Ваш ответ: ' + division + ')' );
+console.log('6 % 3 = ' + (6 % 3) + ' (Ваш ответ: ' + residue + ')' );
 
 
 
